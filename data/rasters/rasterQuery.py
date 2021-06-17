af1e133f570737ec461a487dbfffcaf16bbebd31
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2021-06-17 11:08:12 -0400

    updated raster query

--- data/rasters/rasterQuery.py
+++ data/rasters/rasterQuery.py
@@ -1,30 +1,48 @@
 import numpy as np
 import xarray as xr
+import csv
 
 # info   https://gis.stackexchange.com/questions/299787/finding-pixel-location-in-raster-using-coordinates
 # http://xarray.pydata.org/en/stable/generated/xarray.DataArray.values.html
+
+# read in lat lons from soil_sample CSV
+sampledata = []
+csv_file = 'IL_SoilData.csv'
+
+with open(csv_file, 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        sampledata.append(
+            (row.get('year'), row.get('lon'), row.get('lat'))
+        )
 
 # import file path for raster image 
 filename = 'ICG-Field-3.tif'
 filepath = '/Users/austinarrington/ESMC/data/rasters/'+str(filename)
 
+# headers for new CSV 
+header = ['year', 'lon', 'lat', 'bd']
 
-latlons = [(-88.0293420670401, 39.9299906695464), (-88.03086459, 39.93006724), (-88.03203607, 39.93014691)]
-
-for latlon in latlons:
-    # iterate through array of locations 
-    lon, lat = latlon[0], latlon[1]
-    # read in file 
-    xarr = xr.open_rasterio(filepath)
-    # slice one of the bands
-    img = xarr[0, :, :]
-    #Use the .sel() method to retrieve the value of the nearest cell close to your POI
-    val = img.sel(x=lon, y=lat, method="nearest")
-    # convert arrays data to numpy data array
-    bulk_density = val.values
-    # convert numpy data array to float and round to 4 decimals
-    bd = round(float(bulk_density),4)
-    print("Bulk Density (g/cm3): ", bd, " | ", lat, ",", lon)
-
-
-
+with open('output.csv', 'w', encoding='UTF8', newline='') as f:
+    writer=csv.writer(f)
+    # write header
+    writer.writerow(header)
+    
+    for data in sampledata:
+        year = data[0]
+        # iterate through array of locations
+        lon, lat = data[1], data[2]
+        # read in file 
+        xarr = xr.open_rasterio(filepath)
+        # slice one of the bands
+        img = xarr[0, :, :]
+        #Use the .sel() method to retrieve the value of the nearest cell close to your POI
+        val = img.sel(x=lon, y=lat, method="nearest")
+        # convert arrays data to numpy data array
+        bulk_density = val.values
+        # convert numpy data array to float and round to 4 decimals
+        bd = round(float(bulk_density),4)
+        print("Bulk Density (g/cm3): ", bd, " | ", lat, ",", lon)
+        
+        data = [year, lon, lat, bd]
+        writer.writerow(data)
