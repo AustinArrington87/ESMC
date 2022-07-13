1ba884706594ff1ea7e5507cefaf7fcb21397f98
Austin Arrington <austinbarrington@gmail.com>
2022-07-13 16:54:05 -0400

    Update graph.py

--- api/GraphQL/graph.py
+++ api/GraphQL/graph.py
@@ -4,8 +4,17 @@ import json
 # make post request to Project Names GraphQL Endpoint
 url = "https://gql.esmcportal.org/api/rest/project_names"
 # add API credentials
-header = {'x-hasura-admin-secret': 'EnterSecret'}
+header = {'x-hasura-admin-secret': 'D2FwLafuZdMQuDue7gv66xkD8xuSgW'}
 # call API
 r = requests.get(url, headers=header)
 print(r.json())
 
+
+url2 = "https://gql.esmcportal.org/api/rest/project_names_abbr"
+payload = {'abbr': 'GMP'}
+r = requests.get(url2, headers=header, params=payload)
+print(r.json())
+
+# url2 = "https://gql.esmcportal.org/api/rest/project_names_abbr?abbr=GMP"
+# r = requests.get(url2, headers=header)
+# print(r.json())
