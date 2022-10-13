ed6481f006655a8dc529d563d86387e40544d47c
Austin Arrington <austinbarrington@gmail.com>
2022-10-13 11:54:15 -0400

    Update optis.py
    Fixed Merge function

--- OpTIS/optis.py
+++ OpTIS/optis.py
@@ -2,81 +2,68 @@ import csv
 import pandas as pd
 import numpy as np
 
+
+
+project_years = [2021, 2020, 2019, 2018]
+
+# Load Data
+file1 = 'mrv_data.csv'
+file2 = 'optis_data.csv'
+project_name = "Benson_Hill"
+
 def unique(list1):
 	x = np.array(list1)
 	return(np.unique(x))
 
-# define data frames from MRV and OpTIS
-df_mrv = pd.read_csv('mrv_data.csv', usecols = ['project_name', 'producer_name', 'id', 'field_name', 
+def MergeDataByYear (season, project, mrv_file, opt_file):
+
+	df_mrv = pd.read_csv(mrv_file, usecols = ['project_name', 'producer_name', 'id', 'field_name', 
 	'initial_year', 'season', 'practice_name', 'crop_name'])
-df_opt = pd.read_csv('optis_data.csv', usecols = ['Name', 'Id', 'source', 'year', 'cover_crop', 'conf_index_cover_crop', 
+
+	df_opt = pd.read_csv(opt_file, usecols = ['Name', 'Id', 'source', 'year', 'cover_crop', 'conf_index_cover_crop', 
 	'fall_till_class', 'conf_index_fall_res', 'spring_till_class', 'conf_index_spring_res', 'name'])
 
-#pd.options.display.max_rows = 9999
-print(df_mrv)
-#print(df_mrv.to_string())
+	crops = df_mrv['crop_name']
+	crop_list = crops.to_list()
+	crop_list_unique = unique(crop_list)
+	print(crop_list_unique)
 
-# what crops are we workign with 
-crops = df_mrv['crop_name']
-crop_list = crops.to_list()
-crop_list_unique = unique(crop_list)
-print(crop_list_unique)
+	# unique field IDs (remove duplicate rows from fields with multiple practice changes)
+	ids = df_mrv['id']
+	id_list = ids.to_list()
+	id_list_unique = unique(id_list)
+	print(id_list_unique)
 
-# unique field IDs (remove duplicate rows from fields with multiple practice changes)
-ids = df_mrv['id']
-id_list = ids.to_list()
-id_list_unique = unique(id_list)
-print(id_list_unique)
+	# projects 
+	projects = df_mrv['project_name']
+	project_list = projects.to_list()
+	project_list_unique = unique(project_list)
+	print(project_list_unique)
 
-# projects 
-projects = df_mrv['project_name']
-project_list = projects.to_list()
-project_list_unique = unique(project_list)
-print(project_list_unique)
+	# isolate Benson Hills MRV project 
+	if project == "Benson_Hill":
+		mrv_projName = "Benson Hill"
+		opt_projName = "Benson Hill 2021 Fields"
 
-### Isolate a project --> Benson Hills 
+	MRVprojectFrame = df_mrv.loc[df_mrv['project_name'] == mrv_projName]
+	MRVprojFrameByYear = MRVprojectFrame[MRVprojectFrame['season'] == season]
+	OPTprojectFrame = df_opt.loc[df_opt['source'] == opt_projName]
+	OPTprojFrameByYear = OPTprojectFrame[OPTprojectFrame['year'] == season]
 
-# isolate Benson Hills MRV project 
-Benson_Hill = df_mrv.loc[df_mrv['project_name'] == 'Benson Hill']
-#print(Benson_Hill)
-# only look at rows where there is not a NULL in practice_name
-Benson_Hill_prac = Benson_Hill[Benson_Hill['practice_name'] != '[NULL]']
-#print(Benson_Hill_prac)
-# now isolate frame by year 
-Benson_Hill_2021 = Benson_Hill_prac[Benson_Hill_prac['season'] == 2021]
-print(Benson_Hill_2021)
+	print(MRVprojFrameByYear)
+	print(OPTprojFrameByYear)
 
-
-##### OpTIS Data #####
-#print(df_opt)
-Benson_Hill_Optis = df_opt.loc[df_opt['source'] == 'Benson Hill 2021 Fields']
-#print(Benson_Hill_Optis)
-# isolate frame by year
-Benson_Hill_Optis_2021 = Benson_Hill_Optis[Benson_Hill_Optis['year'] == 2021]
-print(Benson_Hill_Optis_2021)
-
-# check ids 
-#df_new[df_new['l_ext'].isin([31, 22, 30, 25, 64])]
-
-#df1.merge(df2, left_on='objectdesc', right_on='objdescription')[['Content', 'objectdesc', 'TS_id', 'idname']]
-#Benson_Hill_prac.merge(Benson_Hill_Optis, left_on="id", right_on='Id')[['project_name', 'producer_name', 'id', 'field_name', 'season', 'practice_name', 'crop_name']]
-
-BH_2021_Merged = Benson_Hill_2021.merge(Benson_Hill_Optis_2021, left_on="id", right_on='Id')[['project_name', 'producer_name', 'id', 'field_name', 
+	MRV_Opt_Merged = MRVprojFrameByYear.merge(OPTprojFrameByYear, left_on="id", right_on='Id')[['project_name', 'producer_name', 'id', 'field_name', 
 	'initial_year', 'year', 'practice_name', 'crop_name', 'cover_crop', 'conf_index_cover_crop', 
 	'fall_till_class', 'conf_index_fall_res', 'spring_till_class', 'conf_index_spring_res', 'name']]
 
-print(BH_2021_Merged)
-# export DataFrame to CSV
-#BH_2021_Merged.to_csv('BH_2021_Merged.csv', encoding='utf-8')
+	MRV_Opt_Merged.to_csv(project+'_Merged_'+str(season)+'.csv', encoding='utf-8')
 
-# Export CSV of all years (2021-2015), Merging MRV and OpTIS data for Benson Hills project 
-BH_AllYears_Merged = Benson_Hill.merge(Benson_Hill_Optis, left_on="id", right_on='Id')[['project_name', 'producer_name', 'id', 'field_name', 
-	'initial_year', 'year', 'practice_name', 'crop_name', 'cover_crop', 'conf_index_cover_crop', 
-	'fall_till_class', 'conf_index_fall_res', 'spring_till_class', 'conf_index_spring_res', 'name']]
-print(BH_AllYears_Merged)
-BH_AllYears_Merged.to_csv('BH_AllYears_Merged.csv', encoding='utf-8')
+
+# call function 
+
+for year in project_years:
+	MergeDataByYear(year, project_name, file1, file2)
 
 
 # Copyright 2022 ESMC
-
-
